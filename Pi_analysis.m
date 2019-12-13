6e4ceba33f98ed50fa10c65c412897dead69fd8a
Yokesh <58468288+0yokesh@users.noreply.github.com>
2019-12-13 11:36:23 +0000

    Update Pi_analysis.m

--- Pi_analysis.m
+++ Pi_analysis.m
@@ -1,21 +1,29 @@
 %Analysis of number density in Pi digits
 
+%Created by Yokesh
+%Email: yzs0074@auburn.edu
+
 clear;clc;
 close all;
 
 %Import the Pi data text file
-data = fileread("pi_dec_1k.txt");
-data = data(3:end);
+%Read the entire .txt file as a string
+%Beware you should have enough to memory to handle, when to try >1e9 digits
+data = fileread("pi_dec_1m.txt");
+data = data(3:end); %To elminiate 3. at the start
 
-tracker = zeros(10,1);
+tracker = zeros(10,1); %Tracker counts #digit
 n = length(data);
 
 for i = 1:n
-    cc = floor(str2double(data(i)));
-    tracker(cc+1) = tracker(cc+1) + 1;
+
+    %Rips the string and converts into a double data type
+    %Then it is converted to int data type using 'floor'
+    cc = floor(str2double(data(i)));  %I know, but this is the faster way in Matlab
+    tracker(cc+1) = tracker(cc+1) + 1; %Update the tracker
 end
 
-percent = zeros(10,1);
+percent = zeros(10,1); %Percent calculates percentage of occurence of each #digit
 for i = 1:10
     percent(i) = tracker(i)/n*100;
 end
@@ -25,5 +33,5 @@ figure(1);
 plot(0:9, percent, 'k*');
 title("Pi digits - analysis");
 xlabel("#digits");
-% axis([-1 10 9.9 10.2]);
+% axis([-1 10 9.9 10.2]); (optional)
 ylabel("Percentage");
